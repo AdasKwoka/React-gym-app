d80128feefe7a3cfce65d7d61350f9907fa98a37
Adam Kwoka <adam.kwoka99@gmail.com>
2023-08-24 21:29:22 +0200

    feat: display exercise details on detail page

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exercisesOptions, fetchData } from '../utils/fetchData';
+import { exercisesOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([])
   const { id } = useParams();
   
   useEffect(() => {
@@ -16,8 +17,11 @@ const ExerciseDetail = () => {
       const exercisedbUrl = 'https://exercisedb.p.rapidapi.com';
       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
-      const exerciseDetailData = await fetchData(`${exercisedbUrl}/exercises/${id}`, exercisesOptions);
+      const exerciseDetailData = await fetchData(`${exercisedbUrl}/exercises/exercise/${id}`, exercisesOptions);
       setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`, youtubeOptions);
+      setExerciseVideos(exerciseVideosData);
     };
 
     fetchExercisesData();
@@ -25,8 +29,8 @@ const ExerciseDetail = () => {
 
   return (
     <Box>
-      <Detail exerciseDetailData={exerciseDetail} />
-      <ExerciseVideos />
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
       <SimilarExercises />
     </Box>
   )
